953815b6c700ba175d665acd387ee76aac2c3c9c
Erik Johansson <opengd@outlook.com>
2018-10-29 22:01:11 +0100

    Added two more exmaples

---
+++ 10-string.py
@@ -0,0 +1,31 @@
+# 10-string.py
+import time
+import board
+import neopixel
+
+pixel_pin = board.A1 # På vilken pinne sitter pixeln
+num_pixels = 1 # Hur många pixlar
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+
+lista = ["ellen", "pelle", "lena"] # skapa en lista med tre strängar på tre positioner
+
+def lysa(color):
+    pixels.fill(color) # Fyll pixeln med färg
+    pixels.show() # Tänd pixeln
+
+raknare = 0
+while True:
+    namn = lista[raknare]
+    if namn == "ellen": # Om namn är samma som ellen
+        lysa((255, 0, 0))
+    elif namn == "pelle": # Om namn är samma som pelle
+        lysa((0, 255, 0))
+    else: # Vilket annat namn som helst
+        lysa((0, 0, 255))
+    
+    time.sleep(1)
+
+    raknare += 1
+    if raknare == len(lista):
+        raknare = 0
---
+++ 9-array.py
@@ -0,0 +1,23 @@
+# 9-array.py
+import board
+import neopixel
+
+pixel_pin = board.A1 # På vilken pinne sitter pixeln
+num_pixels = 1 # Hur många pixlar
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+
+lista = [0, 0, 0] # skapa en lista med 3 värden på tre positioner
+
+i_max = 256 # max färg värde
+
+def lysa(color):
+    pixels.fill(color) # Fyll pixeln med färg
+    pixels.show() # Tänd pixeln
+
+while True:
+    for i in range(len(lista)): # loop på antalet positioner i arrayen
+        for j in range(i_max): # loopa mellan 0 och 255
+            lista[i] = j # låt position i i listan vara j
+            color = (lista[0], lista[1], lista[2]) # sätt color till de nuvarande värdena i listan
+            lysa(color) # tänd pixeln med färgen
